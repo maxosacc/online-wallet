efddf96fcdc7302b3aaa633a7242c7b0d4d2d406
maxosch <imaksimovic@smartcodehouse.com>
2021-03-26 14:35:47 +0100

    Added efcoreDbContext to Common

---
+++ OnlineWallet/Common/Common.EfCoreDbContext/EfCoreDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Common.EfCoreDbContext
+{
+    public class EfCoreDbContext : DbContext
+    {
+        public EfCoreDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
--- OnlineWallet/Core/Core.ApplicationServices/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Core.ApplicationServices
-{
-    public class Class1
-    {
-    }
-}
