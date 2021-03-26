95e033de122f99e0303d2eaf9482983c72a694f4
maxosch <imaksimovic@smartcodehouse.com>
2021-03-26 13:13:29 +0100

    Inital project commit with solution setup.

---
+++ OnlineWallet/Common/Common.Utils/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Common.Utils
+{
+    public class Class1
+    {
+    }
+}
---
+++ OnlineWallet/Core/Infrastructure/Core.Infrastructure.EfCoreDataAccess/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Core.Infrastructure.EfCoreDataAccess
+{
+    public class Class1
+    {
+    }
+}
