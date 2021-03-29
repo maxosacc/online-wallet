40c8537513c84ed95ba576bc95187b378fe26c5b
maxosch <imaksimovic@smartcodehouse.com>
2021-03-30 01:55:18 +0200

    updated test cases

---
+++ OnlineWallet/Tests/Tests.ApplicationServicesTests/SampleDbContextFactory.cs
@@ -0,0 +1,18 @@
+﻿using Core.Infrastructure.EfCoreDataAccess;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Tests.ApplicationServicesTests
+{
+    public class SampleDbContextFactory : IDesignTimeDbContextFactory<CoreEfCoreDbContext>
+    {
+        public CoreEfCoreDbContext CreateDbContext(string[] args)
+        {
+            var options = new DbContextOptionsBuilder<CoreEfCoreDbContext>()
+                .UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=testDB;Trusted_Connection=True;MultipleActiveResultSets=true")
+            .Options;
+
+            return new CoreEfCoreDbContext(options);
+        }
+    }
+}
--- OnlineWallet/Tests/Tests.ApplicationServicesTests/UnitTest1.cs
+++ OnlineWallet/Tests/Tests.ApplicationServicesTests/UnitTest1.cs
@@ -1,18 +1,26 @@
-using NUnit.Framework;
+using Core.Domain.Repositories;
+using Core.Infrastructure.EfCoreDataAccess;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Tests.ApplicationServicesTests
 {
+    [TestClass]
     public class Tests
     {
-        [SetUp]
+        private ICoreUnitOfWork _coreUnitOfWork;
+        private CoreEfCoreDbContext _dbContext;
+
+        [TestInitialize]
         public void Setup()
         {
+            var dbContextFactory = new SampleDbContextFactory();
+            _dbContext = dbContextFactory.CreateDbContext(new string[] { });
+            _coreUnitOfWork = new CoreEfCoreUnitOfWork(_dbContext);
         }
 
-        [Test]
         public void Test1()
         {
-            Assert.Pass();
+            //Assert.Pass();
         }
     }
 }
