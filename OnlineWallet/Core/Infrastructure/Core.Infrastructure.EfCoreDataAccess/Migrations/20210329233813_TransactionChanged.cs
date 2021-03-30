6fb2663fa268c5db627b048c432a381d419fba6d
maxosch <imaksimovic@smartcodehouse.com>
2021-03-30 02:18:06 +0200

    Added 2 migrations.

---
+++ OnlineWallet/Core/Infrastructure/Core.Infrastructure.EfCoreDataAccess/Migrations/20210329233813_TransactionChanged.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Core.Infrastructure.EfCoreDataAccess.Migrations
+{
+    public partial class TransactionChanged : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
