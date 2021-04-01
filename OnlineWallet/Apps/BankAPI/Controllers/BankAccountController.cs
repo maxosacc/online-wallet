a0a4ce5d18e00b02467046bde988d611ae4df3f7
maxosch <imaksimovic@smartcodehouse.com>
2021-04-01 11:17:26 +0200

    Added BankAPI.

---
+++ OnlineWallet/Apps/BankAPI/Controllers/BankAccountController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace BankAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BankAccountController : Controller
+    {
+        private readonly ILogger<BankAccountController> _logger;
+        public BankAccountController(ILogger<BankAccountController> logger)
+        {
+            _logger = logger;
+        }
+        [HttpPost]
+        [Route("ValidateUserAndGetPassword")]
+        public IActionResult ValidateUserAndGetPassword()
+        {
+            string newPassword = GenerateSixDigitPassword().ToString();
+            _logger.LogInformation("User validated and generated new password.");
+            return Ok(newPassword);
+        }
+
+        private int GenerateSixDigitPassword()
+        {
+            int number = new Random().Next(100000, 999999);
+            _logger.LogInformation("generated six digit password.");
+            return number;
+        }
+    }
+}
---
+++ OnlineWallet/Apps/BankAPI/Controllers/HealthCheckController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace BankAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HealthCheckController : ControllerBase
+    {
+        private readonly ILogger<HealthCheckController> _logger;
+
+        public HealthCheckController(ILogger<HealthCheckController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("CheckStatus")]
+        public IActionResult CheckStatus()
+        {
+            _logger.LogInformation("Status check: healthy");
+            return Ok("healthy");
+        }
+    }
+}
