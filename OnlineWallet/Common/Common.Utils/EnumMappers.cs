ee5b35a79f68e670b3d5966f65befea18637e491
maxosch <imaksimovic@smartcodehouse.com>
2021-04-01 15:50:20 +0200

    Added enumMappers.

---
+++ OnlineWallet/Common/Common.Utils/EnumMappers.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common.Utils
+{
+    public static class EnumMappers
+    {
+        public static string MapBankTypes(int bank)
+        {
+            string result = bank switch
+            {
+                0 => "Xoma bank",
+                _ => throw new ArgumentException("Status not defined for this id")
+            };
+            return result;
+        }
+
+        public static string MapTransactionTypes(int bank)
+        {
+            string result = bank switch
+            {
+                0 => "Uplata",
+                1 => "Isplata",
+                _ => throw new ArgumentException("Status not defined for this id")
+            };
+            return result;
+        }
+    }
+}
