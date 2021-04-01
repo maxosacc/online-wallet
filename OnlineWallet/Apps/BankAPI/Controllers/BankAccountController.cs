53d890741614eae4883092f05c534865bbf749af
maxosch <imaksimovic@smartcodehouse.com>
2021-04-01 15:44:16 +0200

    Bank api updated.

--- OnlineWallet/Apps/BankAPI/Controllers/BankAccountController.cs
+++ OnlineWallet/Apps/BankAPI/Controllers/BankAccountController.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace BankAPI.Controllers
 {
@@ -9,14 +12,20 @@ namespace BankAPI.Controllers
     public class BankAccountController : Controller
     {
         private readonly ILogger<BankAccountController> _logger;
-        public BankAccountController(ILogger<BankAccountController> logger)
+        private readonly List<string> _bankAccounts;
+        private readonly IConfiguration _config;
+        public BankAccountController(ILogger<BankAccountController> logger,
+            IConfiguration config)
         {
             _logger = logger;
+            _config = config;
+            InitializeBankAccounts();
         }
         [HttpPost]
         [Route("ValidateUserAndGetPassword")]
-        public IActionResult ValidateUserAndGetPassword()
+        public IActionResult ValidateUserAndGetPassword(string bankAccountNumber)
         {
+            if (!IsAlreadyExistBankAccount(bankAccountNumber)) return BadRequest("Bank account number does not exist!");
             string newPassword = GenerateSixDigitPassword().ToString();
             _logger.LogInformation("User validated and generated new password.");
             return Ok(newPassword);
@@ -27,6 +36,19 @@ namespace BankAPI.Controllers
             int number = new Random().Next(100000, 999999);
             _logger.LogInformation("generated six digit password.");
             return number;
+        }
+        private bool IsAlreadyExistBankAccount(string bankAccountNumber)
+        {
+            string bankAccount = _bankAccounts.FirstOrDefault(bankAccount => bankAccount == bankAccountNumber);
+            if (bankAccount == bankAccountNumber) return true;
+            return false;
+        }
+        private void InitializeBankAccounts()
+        {
+            _bankAccounts.Add(_config["BankAccounts:Acc1"]);
+            _bankAccounts.Add(_config["BankAccounts:Acc2"]);
+            _bankAccounts.Add(_config["BankAccounts:Acc3"]);
+            _bankAccounts.Add(_config["BankAccounts:Acc4"]);
         }
     }
 }
